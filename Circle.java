a176ad6f1c21e45102c27567303d91ae4314830b
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-07 11:33:40 -0500

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,25 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Circle
+{
+	private double radius;
+	private double area;
+
+	public void setRadius(double rad)
+	{
+	}
+
+	public void calculateArea( )
+	{
+	}
+
+	public void print( )
+	{
+	}
+}
